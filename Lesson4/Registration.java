0e157824dd6a60ccd17971c417ab70c1da044948
Max99966 <maksimhakobjanyan@gmail.com>
2020-11-09 00:53:34 +0400

    Homework for Lesson 4, Model for creating a Gmail account.

---
+++ Lesson4/Registration.java
@@ -0,0 +1,98 @@
+package Lesson4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Registration {
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private int yearOfBirth = 2002;
+    private int monthOfBirth = 01;
+    private int dayOfBirth = 01;
+    private String password;
+    private String password2;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        if (yearOfBirth >= 1905 && yearOfBirth <= 2020) {
+            this.yearOfBirth = yearOfBirth;
+        }
+    }
+
+    public int getMonthOfBirth() {
+        return monthOfBirth;
+    }
+
+    public void setMonthOfBirth(int monthOfBirth) {
+        if (monthOfBirth <= 12 && monthOfBirth >= 1) {
+            this.monthOfBirth = monthOfBirth;
+        }
+    }
+
+    public int getDayOfBirth() {
+        return dayOfBirth;
+    }
+
+    public void setDayOfBirth(int dayOfBirth) {
+        if (dayOfBirth <= 31 && dayOfBirth >= 1) {
+            this.dayOfBirth = dayOfBirth;
+        }
+    }
+
+    public String getEmailAddress() {
+        emailAddress = this.firstName + "." + this.lastName + this.yearOfBirth + "@gmail.com";
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        String l = " ";
+        for (int i = 0; i < password.length(); i++) {
+            l += "* ";
+        }
+        return l;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword2() {
+        String l = " ";
+        for (int i = 0; i < password2.length(); i++) {
+            l += "* ";
+        }
+        return l;
+    }
+
+    public void setPassword2(String password2) {
+        if (password2.equals(password)) {
+            this.password2 = password2;
+        } else {
+            System.out.println("Passwords do not match");
+        }
+    }
+}
---
+++ Lesson4/logicalStructure.java
@@ -0,0 +1,54 @@
+package Lesson4;
+import java.util.Scanner;
+public class logicalStructure {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Registration user = new Registration();
+        String fn;
+        String ln;
+        String pw;
+        String pw2;
+        int y, d, m;
+
+        System.out.println("Please enter your first name.");
+        fn = input.nextLine();
+        user.setFirstName(fn);
+        System.out.println();
+
+        System.out.println("Please enter your last name.");
+        ln = input.nextLine();
+        user.setLastName(ln);
+        System.out.println();
+
+        System.out.println("Please enter your year, month and day of birth");
+        y = input.nextInt();
+        m = input.nextInt();
+        d = input.nextInt();
+        user.setYearOfBirth(y);
+        user.setMonthOfBirth(m);
+        user.setDayOfBirth(d);
+        System.out.println();
+
+        System.out.println("Here is your newly created Email:");
+        System.out.println(user.getEmailAddress());
+        System.out.println();
+
+        //It skips on this part, I don't know the reason for it thou
+        System.out.println("Now, please create a new password");
+        pw = input.nextLine();
+        user.setPassword(pw);
+        System.out.println();
+
+        System.out.println("Please reenter your password");
+        pw2 = input.nextLine();
+        user.setPassword2(pw2);
+        System.out.println();
+
+        System.out.println("You are done!");
+        System.out.println("Here are your inputted information:");
+        System.out.println("First Name: " + user.getFirstName());
+        System.out.println("Last Name: " + user.getLastName());
+        System.out.println("Date of Birth: " + user.getDayOfBirth() + "/" + user.getMonthOfBirth() + "/" + user.getYearOfBirth());
+        System.out.println("Email Address: " + user.getEmailAddress());
+    }
+}
