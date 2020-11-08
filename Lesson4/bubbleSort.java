b4b62cdcb0257096cd4ce42aa09512c823bdb7d0
Max99966 <maksimhakobjanyan@gmail.com>
2020-11-08 18:31:40 +0400

    Homework for Lesson 4, Bubble Sorting an array.

--- Lesson4/bubbleSort.java
+++ Lesson4/bubbleSort.java
@@ -4,7 +4,7 @@ public class bubbleSort {
     public static void main(String[] args) {
         int[] bubbleArray = {5, 2, 10, -10, 3, 0, 1};
         for (int i = 0; i < bubbleArray.length; i++) {
-            for (int j = 0; j < bubbleArray.length - i - 1; j++) {
+            for (int j = 0; j < bubbleArray.length - 1; j++) {
                 if (bubbleArray[j] > bubbleArray[j + 1]) {
                     int t = bubbleArray[j];
                     bubbleArray[j] = bubbleArray[j + 1];
