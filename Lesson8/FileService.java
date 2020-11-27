378c5d542ba23c1d373fd23cb8233431bb250b4d
Max99966 <maksimhakobjanyan@gmail.com>
2020-11-27 19:04:13 +0400

    Homework for lesson 8

---
+++ Lesson8/FileService.java
@@ -0,0 +1,17 @@
+package Lesson8;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class FileService {
+    public static String[] read(String path) throws IOException {
+        return Files.readAllLines(Paths.get(path)).toArray(new String[0]);
+    }
+
+    //Task 1
+    //Print full names of students
+
+
+}
